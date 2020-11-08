b744118208d483338a23f57e8ffe492a25340705
VMFlore <51167227+VMFlore@users.noreply.github.com>
2020-11-08 18:28:17 +0100

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,13 @@
+registerSettingsPage(({ settings }) => (
+  <Page>
+    <Section
+      title={
+        <Text bold align="center">
+          App Settings
+        </Text>
+      }
+    >
+      <Text>AED Guide - BE</Text>
+    </Section>
+  </Page>
+));
